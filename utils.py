5efc7760077f49986a44151c6e81ffef4aa5e3be
Harol Reyes <harolreyes0@gmail.com>
2024-09-11 18:07:27 -0400

    Restruturing the Board class
    Merged the functions to check if the game was a tie and the function to check if the game was won by any of the players.

--- utils.py
+++ utils.py
@@ -38,44 +38,40 @@ class Board:
         """
         self.__board[coordinates] = symbol
 
+    def player_won(self, piece: str) -> bool:
+        """
+        Verifies if the game has been won by the player by checking if there is tree consecutive marks from the same
+        plater in any of the row, columns or diagonals.
+
+        :param player: Player's piece.
+
+        :return: Boolean indicating if the game has been won by the player.
+        """
+
+        # Checks diagonals
+        main_diagonal = lambda board, piece: all([piece == board[i, i] for i in range(board.shape[0])])
+        secondary_diagonal = lambda board, piece: all([piece == board[i, 2 - i] for i in range(board.shape[0])])
+        # Checks if any of the column is full of the palyer's marker columns
+        columns = lambda board, piece: any([True if all(row == piece) else False for row in board.T])
+        # Checks any of the rows is full of the palyer's marker
+        rows = lambda board, piece: any([True if all(row == piece) else False for row in board])
+
+        # Verifying if any of the conditions are fulfilled.
+
+        if any([main_diagonal(self.__board, piece), secondary_diagonal(self.__board, piece),
+                columns(self.__board, piece), rows(self.__board, piece),]):
+            return True
+        else:
+            return False
+
+
+    def tie(self) -> bool:
+        """
+        Verifies if the game is a tie (none of the player won and there is no other move that can be made).
+
+        :return: Boolean indicating if the game is tied.
+        """
+        return all((self.__board != '.').flatten())
+    
     def get_board(self):
         return self.__board
-
-def player_won(marker, board) -> bool:
-    """
-    Verifies if the game has been won by the player by checking if there is tree consecutive marks from the same
-    plater in any of the row, columns or diagonals.
-
-    :param player: Mark of the player in the board.
-    :param board: Game board.
-
-    :return: Boolean indicating if the game has been won by the player.
-    """
-
-    # Checks diagonals
-    main_diagonal = lambda board, marker: all([marker == board[i, i] for i in range(board.shape[0])])
-    secondary_diagonal = lambda board, marker: all([marker == board[i, 2 - i] for i in range(board.shape[0])])
-    # Checks if any of the column is full of the palyer's marker columns
-    columns = lambda board, marker: any([True if all(row == marker) else False for row in board.T])
-    # Checks any of the rows is full of the palyer's marker
-    rows = lambda board, marker: any([True if all(row == marker) else False for row in board])
-
-    # Verifying if any of the conditions are fulfilled.
-
-    if any([main_diagonal(board, marker), secondary_diagonal(board, marker),
-            columns(board, marker), rows(board, marker),]):
-        return True
-    else:
-        return False
-
-
-def tie(board):
-    """
-    Verifies if the game is a tie (none of the player won and there is no other move that can be made).
-
-    :param board: Game board.
-
-    :return: Boolean indicating if the game is tied.
-    """
-    return all((board != 0).flatten())
-
