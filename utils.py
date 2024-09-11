8c2bd9299f219065c46cec16770ed599126ec409
Harol Reyes <harolreyes0@gmail.com>
2024-09-11 17:38:31 -0400

    Moving code from GameBoard.py to utils.py

--- utils.py
+++ utils.py
@@ -1,3 +1,40 @@
+import numpy as np
+
+
+
+
+class Board:
+      """
+      This class creates the game board, this board consist in a 3 x 3 matrix
+      full of zeros.
+      """
+      def __init__(self):
+            self.__board = np.zeros((3, 3), dtype=int)
+
+      def see_board(self):
+            """
+            Prints the game board in an acsii style.
+            """
+            for idx, row in enumerate(self.__board):
+                  print(f'{row[0]} | {row[1]} | {row[2]}')
+                  if (idx + 1) % 3 != 0:
+                        print('- + - + -')
+
+      def add_symbol(self, coordinates, symbol):
+            """
+            Adds the symbol to the cell selected.
+
+            Inputs:
+                  coordinates: coordinates where the symbol is going to be placed.
+                  symbol: symbol to be placed.
+                          player 1: 1
+                          player 2: 2
+            """
+            self.__board[coordinates] = symbol
+
+      def get_board(self):
+            return self.__board
+
 def player_won(marker, board) -> bool:
     """
     Verifies if the game has been won by the player by checking if there is tree consecutive marks from the same
@@ -35,3 +72,4 @@ def tie(board):
     :return: Boolean indicating if the game is tied.
     """
     return all((board != 0).flatten())
+
