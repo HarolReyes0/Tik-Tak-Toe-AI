289f2d5c69c1572bbd5e3046a9588b4828a45205
Harol Reyes <harolreyes0@gmail.com>
2024-09-11 17:36:08 -0400

    Moving code from rules.py to utils.py

--- Experiments.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-matrix = np.zeros((3,3))
-
-for i in range(3):
-    matrix[:,:] = 1
-
-print()
--- utils.py
+++ utils.py
@@ -1,9 +1,37 @@
-import numpy as np
+def player_won(marker, board) -> bool:
+    """
+    Verifies if the game has been won by the player by checking if there is tree consecutive marks from the same
+    plater in any of the row, columns or diagonals.
+
+    :param player: Mark of the player in the board.
+    :param board: Game board.
+
+    :return: Boolean indicating if the game has been won by the player.
+    """
+
+    # Checks diagonals
+    main_diagonal = lambda board, marker: all([marker == board[i, i] for i in range(board.shape[0])])
+    secondary_diagonal = lambda board, marker: all([marker == board[i, 2 - i] for i in range(board.shape[0])])
+    # Checks if any of the column is full of the palyer's marker columns
+    columns = lambda board, marker: any([True if all(row == marker) else False for row in board.T])
+    # Checks any of the rows is full of the palyer's marker
+    rows = lambda board, marker: any([True if all(row == marker) else False for row in board])
+
+    # Verifying if any of the conditions are fulfilled.
+
+    if any([main_diagonal(board, marker), secondary_diagonal(board, marker),
+            columns(board, marker), rows(board, marker),]):
+        return True
+    else:
+        return False
 
 
-matrix = np.zeros((3,3))
+def tie(board):
+    """
+    Verifies if the game is a tie (none of the player won and there is no other move that can be made).
 
-for i in range(3):
-    matrix[:,:] = 1
+    :param board: Game board.
 
-print()
+    :return: Boolean indicating if the game is tied.
+    """
+    return all((board != 0).flatten())
